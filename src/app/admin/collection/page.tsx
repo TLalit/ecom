a62d1ed2aa935f317e1bceba189af7d52e34e061
sashwat <sashwattomar777@example.com>
2024-07-04 11:49:14 +0530

    fix: remove comments

--- src/app/admin/collection/page.tsx
+++ src/app/admin/collection/page.tsx
@@ -100,37 +100,6 @@ const columns: DataTableProps<
       cell: ({ row }) => {
         return (
           <ActionsDropdown row={row} />
-          // <DropdownMenu>
-          //   <DropdownMenuTrigger className="p-2">
-          //     <LucideIcon name="EllipsisVertical" />
-          //   </DropdownMenuTrigger>
-          //   <DropdownMenuContent>
-          //     <DropdownMenuItem>
-          //       <LucideIcon name="PenLine" />
-          //       <span>Edit</span>
-          //     </DropdownMenuItem>
-          //     <DropdownMenuItem>
-          //       <LucideIcon name="SquarePlus" />
-          //       <span>Add Products</span>
-          //     </DropdownMenuItem>
-          //     <DropdownMenuItem>
-          //       <LucideIcon name="Eye" />
-          //       <span>View Products</span>
-          //     </DropdownMenuItem>
-          //     <DropdownMenuItem>
-          //       <LucideIcon name="Info" />
-          //       <span>Details</span>
-          //     </DropdownMenuItem>
-          //     <DropdownMenuSeparator />
-          //     <DropdownMenuItem
-          //       destructive
-          //       onClick={() => confirmBeforeAction(async () => {})}
-          //     >
-          //       <LucideIcon name="Trash" />
-          //       <span>Delete</span>
-          //     </DropdownMenuItem>
-          //   </DropdownMenuContent>
-          // </DropdownMenu>
         );
       },
     },
